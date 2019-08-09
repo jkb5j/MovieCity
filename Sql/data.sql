9082be973952f76da3548adb8f401135a43cca1d
BrentonByrd <fosterbyrdbrenton@gmail.com>
2019-08-09 14:23:27 -0400

    added genres

--- Sql/data.sql
+++ Sql/data.sql
@@ -1,3 +1,9 @@
 INSERT INTO roles (role_name)
 VALUES ('admin'), ('user');
 
+INSERT INTO genres (genre_type)
+VALUES ('action'), ('family'), ('horror'), ('sci-fi'), ('animation'),
+('anime'), ('drama'), ('thriller'), ('comedy'), ('musical'),
+('anthology film'), ('biopic'), ('romance'), ('adventure'),
+('crime'), ('documentary'), ('mystery'), ('superhero'), ('suspense'),
+('western'), ('zombie');
