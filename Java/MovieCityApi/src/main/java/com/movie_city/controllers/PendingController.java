9ea368bd27ae8fc9b22c4f61a374a7d04d1023bd
BrettDavis1 <2014BrettDavis@gmail.com>
2019-08-14 18:11:09 -0400

    added pending endpoint

--- Java/MovieCityApi/src/main/java/com/movie_city/controllers/PendingController.java
+++ Java/MovieCityApi/src/main/java/com/movie_city/controllers/PendingController.java
@@ -1,5 +1,23 @@
 package com.movie_city.controllers;
 
-public class PendingController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.movie_city.models.Pending;
+import com.movie_city.services.PendingService;
+
+@RestController
+@RequestMapping("Pending")
+public class PendingController {
+	@Autowired
+	private PendingService ps;
+	
+	@GetMapping
+	private List<Pending> findAll() {
+		return ps.findAll();
+	}
 }
