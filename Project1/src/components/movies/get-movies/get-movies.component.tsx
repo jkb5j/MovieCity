898aa0dd6541db16be2cd3b8acc3d86df59b36fa
Joseph <jkb5j@mtmail.mtsu.edu>
2019-08-15 17:00:55 -0400

    Added plot button

--- Project1/src/components/movies/get-movies/get-movies.component.tsx
+++ Project1/src/components/movies/get-movies/get-movies.component.tsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button } from 'reactstrap';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Movie } from '../../../models/movie';
 import { Genre } from '../../../models/genre';
 import { environment } from '../../../environment';
+import { Modal, Alert } from 'reactstrap';
+import { async } from 'q';
+
 
 interface IState {
     movies: Movie[],
@@ -10,7 +13,9 @@ interface IState {
     genreDropdown: {
         isOpen: boolean,
         selection: string
-    }
+    },
+    plotModalShow: boolean,
+    currentSelectedPlot?: string
 }
 
 export default class GetMovies extends Component<{}, IState> {
@@ -22,7 +27,8 @@ export default class GetMovies extends Component<{}, IState> {
             genreDropdown: {
                 isOpen: false,
                 selection: 'All Movies'
-            }
+            },
+            plotModalShow: false
         };
     }
 
@@ -44,20 +50,6 @@ export default class GetMovies extends Component<{}, IState> {
             }
         });
     }
-
-    // getMyReimbursements = async () => {
-    //     const resp = await fetch(environment.context + '/reimb/reimb/author/0', {
-    //         credentials: 'include'
-    //     });
-    //     const reimbsFromServer = await resp.json();
-    //     this.setState({
-    //         reimbs: reimbsFromServer,
-    //         statusDropdown: {
-    //             ...this.state.statusDropdown,
-    //             selection: 'My Reimbursements'
-    //         }
-    //     });
-    // }
 
     getGenre = async () => {
         const resp = await fetch(environment.context + '/genres');
@@ -82,21 +74,6 @@ export default class GetMovies extends Component<{}, IState> {
         })
     }
 
-    //might need to make a find plot by movie id 
-    // getMoviesPlots = async (genre: Genre) => {
-    //     const resp = await fetch(environment.context + '/movies/filter/genre/' + genre.genreId, {
-    //         credentials: 'include'
-    //     });
-    //     const moviesFromServer = await resp.json();
-    //     this.setState({
-    //         movies: moviesFromServer,
-    //         genreDropdown: {
-    //             ...this.state.genreDropdown,
-    //             selection: genre.genre
-    //         }
-    //     })
-    // }
-
     toggleGenreDropdown = () => {
         this.setState({
             genreDropdown: {
@@ -106,33 +83,47 @@ export default class GetMovies extends Component<{}, IState> {
         })
     }
 
+    togglePlotModalShow = () => {
+        this.setState({
+            plotModalShow: !this.state.plotModalShow
+        })
+    }
+
+    selectPlot = (plot: string) => {
+        this.setState({
+            plotModalShow: true,
+            currentSelectedPlot: plot
+        })
+    }
+
+
     render() {
         const movies = this.state.movies;
-        return(
+        return (
             <div id="reimb-table-container">
                 <table className="table table-striped table-dark">
                     <thead>
                         <tr>
                             <th scope="col">Title</th>
                             <th scope="col"><ButtonDropdown id="reimb-status-dropdown"
-                    isOpen={this.state.genreDropdown.isOpen}
-                    toggle={this.toggleGenreDropdown}>
-                        <DropdownToggle caret>
-                            {this.state.genreDropdown.selection}
-                        </DropdownToggle>
-                        <DropdownMenu right>
-        <DropdownItem onClick={this.getAllMovies}>All Movies</DropdownItem>
-                            <DropdownItem divider />
-                            {
-                                this.state.genre.map(genre => (
-                                    <DropdownItem key={'status-dropdown-' + genre.genreId}
-                                    onClick={() => this.getMoviesByGenre(genre)}>
-                                        {genre.genre}
-                                    </DropdownItem>
-                                ))
-                            }
-                        </DropdownMenu>
-                    </ButtonDropdown></th>
+                                isOpen={this.state.genreDropdown.isOpen}
+                                toggle={this.toggleGenreDropdown}>
+                                <DropdownToggle caret>
+                                    {this.state.genreDropdown.selection}
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem onClick={this.getAllMovies}>All Movies</DropdownItem>
+                                    <DropdownItem divider />
+                                    {
+                                        this.state.genre.map(genre => (
+                                            <DropdownItem key={'status-dropdown-' + genre.genreId}
+                                                onClick={() => this.getMoviesByGenre(genre)}>
+                                                {genre.genre}
+                                            </DropdownItem>
+                                        ))
+                                    }
+                                </DropdownMenu>
+                            </ButtonDropdown></th>
                             <th scope="col">Release Year</th>
                             <th scope="col">Origin</th>
                             <th scope="col">Director</th>
@@ -142,18 +133,30 @@ export default class GetMovies extends Component<{}, IState> {
                     <tbody>
                         {
                             movies.map(movie =>
-                                <tr key={'movieId-'+movie.movieId}>
+                                <tr key={'movieId-' + movie.movieId}>
                                     <td>{movie.title}</td>
                                     <td>{movie.genre.genre}</td>
                                     <td>{movie.releaseYear}</td>
                                     <td>{movie.origin}</td>
                                     <td>{movie.director}</td>
-                                    <td><Button id="veiw-plot">Veiw Plot</Button> </td>
+                                    <td><Button className="plot" type="button" class="btn btn-primary" onClick={() => this.selectPlot(movie.plot)}>
+                                        Plot
+                                                </Button></td>
                                 </tr>
-                                )
+                            )
                         }
                     </tbody>
                 </table>
+                <Modal isOpen={this.state.plotModalShow} toggle={this.togglePlotModalShow} >
+                    <ModalHeader toggle={this.togglePlotModalShow}>Modal title</ModalHeader>
+                    <ModalBody>
+                        {this.state.currentSelectedPlot}
+          </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.togglePlotModalShow}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.togglePlotModalShow}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
             </div>
         )
     }
