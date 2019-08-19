a48463cd06fec1e3b37e7b9518325fd2560eaf27
BrettDavis1 <2014BrettDavis@gmail.com>
2019-08-19 16:45:55 -0400

    unfavorite movie component

--- Project1/src/components/bottons-component/unfavorite/unfavorite.component.tsx
+++ Project1/src/components/bottons-component/unfavorite/unfavorite.component.tsx
@@ -1,1 +1,38 @@
 import React, { Component } from 'react';
+import { environment } from '../../../environment';
+import { Button } from 'reactstrap';
+
+interface IState {
+    favMovie: Number
+}
+
+export default class GetFavoriteMovie extends Component<{}, IState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            favMovie: 0
+        };
+    }
+
+    unfavoriteMovie = async (favMovieId: Number) => {
+        this.setState({
+            favMovie: favMovieId
+        });
+        const resp = await fetch(environment.context + '/users/favorites/' + '1'/*logged in user*/+'/movie/' + favMovieId,{
+                method: 'DELETE',
+                credentials: 'include',
+                headers: {
+                    'content-type': 'application/json'
+                }
+        });
+    }
+    render() {
+        const pendings = this.state.favMovie;
+        return(
+            <Button className="btn btn-primary" type="button" 
+                onClick={() => this.unfavoriteMovie(this.state.favMovie)}>
+                    Unfavorite Movie
+                    </Button>
+        )
+    }
+}
