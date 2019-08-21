ac5962eebc5c382b2cd89d7c440f97367f79efce
Joseph <jkb5j@mtmail.mtsu.edu>
2019-08-21 16:31:18 -0400

    edit yml

--- Java/MovieCityApi/src/test/java/com/movie_city/MovieCityApiApplicationTests.java
+++ Java/MovieCityApi/src/test/java/com/movie_city/MovieCityApiApplicationTests.java
@@ -1,9 +1,10 @@
 package com.movie_city;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
